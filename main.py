4782dd26be1b063f146d50b33b9e6467e374f3e6
Maida <maida.maxime@gmail.com>
2021-03-03 21:24:45 +0100

    Création du RecycleView // Affichage du nom de pizza

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, StringProperty
 
 from models import Pizza
 
 
 class PizzaWidget(BoxLayout):
-    pass
+    nom = StringProperty()
 
 class MainWidget(BoxLayout):
+    recycleView = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.Pizzas = [
             Pizza("4 fromages", "chèvre, emmental, brie, comté", 9.5, True),
             Pizza("Chorizo", "tomates, chorizo, parmesan", 11.2, False),
             Pizza("Calzone", "fromage, jambon, champignons", 10, False),
+        ]
+
+    def on_parent(self, widget, parent):
+        self.recycleView.data = [
+            {"nom": "4 fromages"},
+            {"nom": "Chorizo"}
         ]
 
 
