fede059295a25102ad531e31206f34598ff6ae40
Maida <maida.maxime@gmail.com>
2021-03-03 21:56:48 +0100

    Affichage de toutes les  infos Pizza

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
 
 from models import Pizza
 
 
 class PizzaWidget(BoxLayout):
     nom = StringProperty()
+    ingredients = StringProperty()
+    prix = NumericProperty()
+    vegetarienne = BooleanProperty()
 
 class MainWidget(BoxLayout):
     recycleView = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.Pizzas = [
+        self.pizzas = [
             Pizza("4 fromages", "chèvre, emmental, brie, comté", 9.5, True),
             Pizza("Chorizo", "tomates, chorizo, parmesan", 11.2, False),
             Pizza("Calzone", "fromage, jambon, champignons", 10, False),
         ]
 
     def on_parent(self, widget, parent):
-        self.recycleView.data = [
-            {"nom": "4 fromages"},
-            {"nom": "Chorizo"}
-        ]
+        l = [pizza.get_dictionary() for pizza in self.pizzas]
+        self.recycleView.data = l
 
 
 class PizzaApp(App):
